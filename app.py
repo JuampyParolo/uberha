332aba97ba9ffa90cfdbb83172519d9bc4843bad
JuampyParolo <112802534+JuampyParolo@users.noreply.github.com>
2025-04-23 01:44:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -36,3 +36,9 @@ def callback():
 
     response = requests.post(token_url, data=data)
     return response.json()
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
